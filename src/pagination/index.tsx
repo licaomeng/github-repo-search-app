5c49ae6828a778e224a58b16a82ebd64a5de4dc6
Messi Li <1967432704@qq.com>
2022-11-20 03:31:55 +0800

    tune pagination

--- src/pagination/index.tsx
+++ src/pagination/index.tsx
@@ -7,6 +7,17 @@ interface PaginationProps {
     totalPages: number;
 }
 
+function navBtn(isNext: boolean, isDisabled: boolean) {
+    const text = isNext ? "Next >" : "< Prev";
+    const style = isNext ? styles.nextPage : styles.prevPage;
+    
+    if (isDisabled) {
+        return <span className={cx(styles.btn, style, styles.disabled)}>{text}</span>;
+    }
+
+    return <a className={cx(styles.btn, style)}>{text}</a>;
+}
+
 export function Pagination(props: PaginationProps) {
     const { currentPage, totalPages } = props;
 
@@ -14,25 +25,24 @@ export function Pagination(props: PaginationProps) {
         return <a className={currentPage === i + 1 ? cx(styles.btn, styles.selected) : styles.btn}>{++i}</a>;
     }
 
-    function renderNavigator() {
+    function renderNav() {
         if (totalPages < 10) {
-            return [...Array(totalPages)].map((_, i) => {
-                return populateItem(i);
-            });
+            // no ...
+            return [...Array(totalPages)].map((_, i) => populateItem(i));
         }
 
         return [...Array(totalPages)].map((_, i) => {
             if (currentPage < 6 || currentPage > totalPages - 2) {
+                // like 1 2 3 4 ⑤ ... 6 7
                 if (i === 5) return "...";
                 if (i > 5 && i < totalPages - 2) return null;
             } else {
-                if (Math.abs(currentPage - (i + 1)) < 2 ||
-                 i < 2 ||
-                  i > totalPages - 3
-                  ) {
-                    return populateItem(i);
-                }
-                if (i === 3 || i === totalPages - 3) return "...";
+                // like 1 2 ... 6 ⑦ 8 ... 14 15
+                const isNav = Math.abs(currentPage - (i + 1)) < 2 || i < 2 || i > totalPages - 3;
+                const isEllipse = ((i === 3) || (i === totalPages - 3));
+                
+                if (isNav) return populateItem(i);
+                if (isEllipse) return "...";
 
                 return null;
             }
@@ -47,17 +57,11 @@ export function Pagination(props: PaginationProps) {
 
     return (
         <div className={styles.paginationContainer}>
-            {
-                currentPage === 1 ?
-                    <span className={cx(styles.btn, styles.prevPage, styles.disabled)}>&lt; prev</span> :
-                    <a className={cx(styles.btn, styles.prevPage)}>&lt; prev</a>
-            }
-            {renderNavigator()}
-            {
-                currentPage === totalPages ?
-                    <span className={cx(styles.btn, styles.nextPage, styles.disabled)}>next &gt;</span> :
-                    <a className={cx(styles.btn, styles.nextPage)}>next &gt;</a>
-            }
+            { currentPage === 1 ? navBtn(false, true) : navBtn(false, false) }
+            <div className={styles.nav}>
+                {renderNav()}
+            </div>
+            { currentPage === totalPages ? navBtn(true, true) : navBtn(true, false) }
         </div>
     );
 }
