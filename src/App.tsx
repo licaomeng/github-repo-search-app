b197c0837430856dfe8cd2c841d5d439816af401
Messi Li <1967432704@qq.com>
2022-11-21 06:32:38 +0800

    add loadbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,10 @@ import { SearchBox } from "./search-box";
 import { Octokit } from "@octokit/core";
 import { useSearchParams } from "react-router-dom";
 import { throttle } from "./utils";
+import LoadingBar from "react-top-loading-bar";
 import "./App.css";
 
-const octokit = new Octokit();
+const octokit = new Octokit({ auth: "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" });
 const PER_PAGE = 5;
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
   const [searchParams] = useSearchParams();
   const [_, setQuery] = useState("");
   const [totalCount, setTotalCount] = useState(0);
+  const [progress, setProgress] = useState(0);
+  let intervalId = 0;
 
   const fetchThrottle = throttle(octokit.request, 1000);
 
@@ -24,21 +27,30 @@ function App() {
     if (q === "") {
       return;
     }
+
+    setProgress(0);
+    intervalId = window.setInterval(() => setProgress((progress) => progress + 10), 500);
     const { data } = await fetchThrottle("GET /search/repositories?q={q}&per_page={per_page}", {
       q,
       page,
       per_page
     });
 
+    clearInterval(intervalId);
+    setProgress(100);
+
     console.log(data);
 
-    setRepoList((list) => data.items);
+    setRepoList(data.items);
     setTotalCount(data.total_count);
   }
 
   useEffect(() => {
-    fetchRepo();
-  }, []);
+
+    return () => {
+      window.clearInterval(intervalId);
+    };
+}, []);
 
   // pass searchParams as a dependency into the useEffect
   useEffect(() => {
@@ -57,6 +69,11 @@ function App() {
 
   return (
     <>
+      <LoadingBar
+        height={3}
+        color="#0969da"
+        progress={progress}
+      />
       <Header><SearchBox isInHeader={true}></SearchBox></Header>
       <span id="search-box"><SearchBox isInHeader={false}></SearchBox></span>
       <div className="total-count">{totalCount.toLocaleString()} available repository results</div>
