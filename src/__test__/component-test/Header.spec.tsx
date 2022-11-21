f33727ef93c3fd9766fb5a678f01324b8ad7e733
Messi Li <1967432704@qq.com>
2022-11-21 08:02:07 +0800

    CT

--- src/__test__/component-test/Header.spec.tsx
+++ src/__test__/component-test/Header.spec.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
-import { Header } from "../header";
+import { Header } from "../../header";
 
 test("[Desktop]renders the header component", () => {
     Object.defineProperty(window, "innerHeight", {
--- src/__test__/component-test/Pagination.spec.tsx
+++ src/__test__/component-test/Pagination.spec.tsx
@@ -1,70 +1,71 @@
 import * as React from "react";
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
-import { Pagination } from "../pagination";
+import { Pagination } from "../../pagination";
+import { BrowserRouter as Router } from "react-router-dom";
 
 test("renders the pagination component: currentPage={7} totalPages={15}", () => {
-  const view = render(<Pagination currentPage={7} totalPages={15}></Pagination>);
+  const view = render(<Router><Pagination currentPage={7} totalPages={15}></Pagination></Router>);
 
   expect(view).toMatchSnapshot();
 });
 
 test("renders the pagination component: currentPage={1} totalPages={1}", () => {
-  const view = render(<Pagination currentPage={1} totalPages={1}></Pagination>);
+  const view = render(<Router><Pagination currentPage={1} totalPages={1}></Pagination></Router>);
 
   expect(view).toMatchSnapshot();
 });
 
 test("renders the pagination component: currentPage={1} totalPages={9}", () => {
-  const view = render(<Pagination currentPage={1} totalPages={9}></Pagination>);
+  const view = render(<Router><Pagination currentPage={1} totalPages={9}></Pagination></Router>);
 
   expect(view).toMatchSnapshot();
 });
 
 test("renders the pagination component: currentPage={5} totalPages={9}", () => {
-  const view = render(<Pagination currentPage={5} totalPages={9}></Pagination>);
+  const view = render(<Router><Pagination currentPage={5} totalPages={9}></Pagination></Router>);
 
   expect(view).toMatchSnapshot();
 });
 
 test("renders the pagination component: currentPage={9} totalPages={9}", () => {
-  const view = render(<Pagination currentPage={9} totalPages={9}></Pagination>);
+  const view = render(<Router><Pagination currentPage={9} totalPages={9}></Pagination></Router>);
 
   expect(view).toMatchSnapshot();
 });
 
 test("renders the pagination component: currentPage={5} totalPages={10}", () => {
-  const view = render(<Pagination currentPage={5} totalPages={10}></Pagination>);
+  const view = render(<Router><Pagination currentPage={5} totalPages={10}></Pagination></Router>);
 
   expect(view).toMatchSnapshot();
 });
 
 test("renders the pagination component: currentPage={10} totalPages={10}", () => {
-  const view = render(<Pagination currentPage={10} totalPages={10}></Pagination>);
+  const view = render(<Router><Pagination currentPage={10} totalPages={10}></Pagination></Router>);
 
   expect(view).toMatchSnapshot();
 });
 
 test("renders the pagination component: currentPage={10} totalPages={15}", () => {
-  const view = render(<Pagination currentPage={10} totalPages={15}></Pagination>);
+  const view = render(<Router><Pagination currentPage={10} totalPages={15}></Pagination></Router>);
 
   expect(view).toMatchSnapshot();
 });
 
 test("renders the pagination component: currentPage={11} totalPages={15}", () => {
-  const view = render(<Pagination currentPage={11} totalPages={15}></Pagination>);
+  const view = render(<Router><Pagination currentPage={11} totalPages={15}></Pagination></Router>);
 
   expect(view).toMatchSnapshot();
 });
 
 test("renders the pagination component: currentPage={14} totalPages={15}", () => {
-  const view = render(<Pagination currentPage={14} totalPages={15}></Pagination>);
+  const view = render(<Router><Pagination currentPage={14} totalPages={15}></Pagination></Router>);
 
   expect(view).toMatchSnapshot();
 });
 
 test("renders the pagination component: currentPage={5} totalPages={15}", () => {
-  const view = render(<Pagination currentPage={5} totalPages={15}></Pagination>);
+  const view = render(<Router><Pagination currentPage={5} totalPages={15}></Pagination></Router>);
 
   expect(view).toMatchSnapshot();
 });
